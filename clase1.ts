00d1fcd1af381b98c7b2145ea852952e04486179
Sebastian Ramirez <ramirezmatias946@gmail.com>
2021-02-24 11:32:20 -0300

    Update clase1.ts
    Agregamos contenido de la clase 1

---
+++ clase1.ts
@@ -0,0 +1,143 @@
+
+//Variables
+var nombre = ''; //Ya no se usa mas
+
+//Usar!!!!
+//Variable temporal osea ser modificado
+let nombre1 = '';
+
+nombre1 = '12';
+
+const nombre2 = ''
+
+let a = function () {
+    let nombre2 = ''
+    const nombre1 = ''
+}
+
+
+//Tipo de Datos
+//Numeros
+let numero: number;
+
+//Cadenas
+let apellido: string;
+
+//booleans - Verdadero o falso
+let esRico: boolean;
+
+let variables: any;
+
+
+//Variables
+var nombre = ''; //Ya no se usa mas
+
+//Usar!!!!
+//Variable temporal osea ser modificado
+let nombre1 = '';
+
+nombre1 = '12';
+
+const nombre2 = ''
+
+//Tipo de Datos
+//Numeros
+let numero: number;
+
+//Cadenas
+let apellido: string;
+
+//booleans - Verdadero o falso
+let esRico: boolean;
+
+//Any se puede cambiar a cualquer tipo de dato
+let variables: any;
+variables = '';
+variables = 1;
+variables = false;
+
+
+//Operaciones Matematica
+numero = 100;
+let suma = 100 + 9000;
+let resta = 100 - 9000;
+let multi = 9000 * 100;
+let division = 100 / 3
+let residuo = 100 % 2;
+console.log(suma)
+
+console.log(resta)
+
+console.log(multi)
+
+console.log(division)
+
+//Variables
+var nombre = ''; //Ya no se usa mas
+
+//Usar!!!!
+//Variable temporal osea ser modificado
+let nombre1 = '';
+
+nombre1 = '12';
+
+const nombre2 = ''
+
+//Tipo de Datos
+//Numeros
+let numero: number;
+
+//Cadenas
+let apellido: string;
+
+//booleans - Verdadero o falso
+let esRico: boolean;
+
+//Any se puede cambiar a cualquer tipo de dato
+let variables: any;
+variables = '';
+variables = 1;
+variables = false;
+
+
+//Operaciones Matematica
+numero = 100;
+let suma = 100 + 9000;
+let resta = 100 - 9000;
+let multi = 9000 * 100;
+let division = 100 / 3
+let residuo = 49 % 7;
+console.log(suma)
+
+console.log(resta)
+
+console.log(multi)
+
+console.log(division)
+
+console.log(residuo)
+
+//Operadores para comparar
+//=== Igual
+// > Mayor
+// >= Mayor o igual
+//< Menor
+//<= Menoro igual
+//!== Distinto
+
+let a = 1===1;
+let b = 1>=4
+let r = 34 !== 34
+
+
+//Operadores Logicos
+//And(y)
+//Or(o)
+//Not(negado)
+
+
+
+
+
+
+
